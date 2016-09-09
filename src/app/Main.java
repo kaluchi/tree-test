6e8c03d59a60f9ec1756337ac665f5ad0d67d752
chuev.ruslan <chuev.work@gmail.com>
2016-09-09 18:38:38 +0300

    Здравствуйте, Евгений Васильевич! Рад представить Вам это решение Вашей задачи.

--- src/app/Main.java
+++ src/app/Main.java
@@ -6,16 +6,50 @@ package app;
  *
  */
 public class Main {
-	
+
 	public static void main(String[] args) {
 		int maxTreeDepth = getMaxTreeDepth(NodeImpl.testCase01());
 		System.out.println(maxTreeDepth);
 	}
-	
-	
+
 	public static int getMaxTreeDepth(Node arbitraryNode) {
-		// TODO: нужно посчитать максимальную грубину дерева, в котором находится переданный узел
-		return 0;
+		return getDeepestNode(getRoot(arbitraryNode));
+	}
+
+	public static Node getRoot(Node node) {
+		Node root = node;
+
+		while (root.getParent() != null) {
+			root = root.getParent();
+		}
+
+		return root;
+	}
+
+	public static int getDeepestNode(Node node) {
+		int max = 0;
+
+		if(node.getChildren().isEmpty()) {
+			max = Math.max(max, getLevel(node));
+		}
+		else {
+			for(Node child : node.getChildren()) {
+				max = Math.max(getDeepestNode(child), max);
+			}
+		}
+
+		return max;
+	}
+
+	public static int getLevel(Node node) {
+		int level = 0;
+
+		while (node.getParent() != null) {
+			node = node.getParent();
+			level++;
+		}
+
+		return level;
 	}
 
 }
